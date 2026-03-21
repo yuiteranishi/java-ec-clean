a80820340de8bf1cd7322bcfda7d67304cbbb1f2
yuiteranishi <codetrain.yui.teranishi@gmail.com>
2026-03-21 18:42:13 +0900

    不要なアノテーションを削除

--- src/main/java/com/example/ec/infrastructure/product/ProductJpaEntity.java
+++ src/main/java/com/example/ec/infrastructure/product/ProductJpaEntity.java
@@ -19,31 +19,22 @@ public class ProductJpaEntity {
     private Long id;
 
     /** 商品名（必須） */
-    @NotBlank
-    @Size(max = 255)
     @Column(nullable = false)
     private String name;
 
     /** 価格（必須） */
-    @NotNull
-    @DecimalMin(value = "0.0", inclusive = false, message = "validation.price,positive")
-    @Digits(integer = 8, fraction = 2)
     @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal price;
 
     /** 説明（任意） */
-    @Size(max = 1000)
     @Column(length = 1000)
     private String description;
 
     /** 画像（任意） */
-    @URL(message = "validation.url.invalid")
     @Column(length = 1024)
     private String imageUrl;
 
     /** 在庫数（必須） */
-    @NotNull
-    @PositiveOrZero
     @Column(nullable = false)
     private Integer stockQty = 0;
 
