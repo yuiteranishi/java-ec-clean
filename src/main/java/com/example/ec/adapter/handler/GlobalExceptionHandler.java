bc14393e31225b241a567872650376ceb7d14974
yuiteranishi <codetrain.yui.teranishi@gmail.com>
2026-04-20 20:05:04 +0900

    [codeRabbit指摘]例外発生時のステータスコードを修正

--- src/main/java/com/example/ec/adapter/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/ec/adapter/handler/GlobalExceptionHandler.java
@@ -2,14 +2,17 @@ package com.example.ec.adapter.handler;
 
 import com.example.ec.domain.product.exception.CategoryNotFoundException;
 import com.example.ec.domain.product.exception.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ProductNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleProductNotFound(ProductNotFoundException ex, Model model)
     {
         model.addAttribute("message", ex.getMessage());
@@ -18,6 +21,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(CategoryNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleCategoryNotFound(CategoryNotFoundException ex, Model model)
     {
         model.addAttribute("message", ex.getMessage());
